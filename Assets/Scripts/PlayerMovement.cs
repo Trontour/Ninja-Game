a3b1a0d2e600151b285e0a0a7e0c83489ab8bdc7
Trontour Wang <trontourhuiyuanwang@gmail.com>
2022-07-19 10:47:17 -0700

    Working jump and collisions

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,17 +5,42 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private bool isGrounded = true;
+    private bool hasJumped = false;
     public float jumpForce = 500f;
+    public Transform body;
     // Start is called before the first frame update
     public void Jump(InputAction.CallbackContext context)
     {
         if (context.performed)
         {
-            Debug.Log("Jump");
-            gameObject.GetComponent<Rigidbody>().AddForce(0, jumpForce, 0);
+            if(isGrounded == true)
+            {
+                Debug.Log("Jump");
+                gameObject.GetComponent<Rigidbody>().AddForce(0, jumpForce, 0);
+             
+            }
+            
         }
     }
-    void Start()
+    void GroundCheck()
+    {
+        RaycastHit hit;
+        float distance = .55f;
+        Vector3 dir = new Vector3(0, -1, 0);
+
+        if (Physics.Raycast(body.transform.position, dir, out hit, distance))
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+        
+    }
+
+void Start()
     {
         
     }
@@ -23,6 +48,7 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        GroundCheck();
+        Debug.Log(isGrounded);
     }
 }
