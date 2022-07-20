10c8fae220dca8aa301755271fd4a7c231774085
jairskyon <109252709+jairskyons@users.noreply.github.com>
2022-07-20 11:03:31 -0700

    import p1

---
+++ Assets/CityBuilderSciFi/City Builder Cyberpunk/Scenes/Scripts/OptimiserLightFade.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.HighDefinition;
+
+[RequireComponent(typeof(Light))]
+
+
+public class OptimiserLightFade : MonoBehaviour
+{
+
+    private HDAdditionalLightData lightData;
+    public float StartFalloffDist = 5.0f;
+    public float falloffSpeed =1.0f;
+    public float dist;
+    public float range;
+
+    private float StartRange;
+    void Start()
+    {
+        lightData = GetComponent<HDAdditionalLightData>();
+        StartRange = lightData.range;
+    }
+
+    void Update()
+    {
+        dist = Vector3.Distance(Camera.main.transform.position, transform.position);
+        float falloffMulti = dist * falloffSpeed;
+        float Setrange = StartFalloffDist * falloffSpeed + StartRange;
+        range = Setrange - falloffMulti;
+
+        if (dist > StartFalloffDist)
+        {
+
+            lightData.range = range;
+        }
+        else
+        {
+            lightData.range = StartRange;
+        }
+    }
+}
